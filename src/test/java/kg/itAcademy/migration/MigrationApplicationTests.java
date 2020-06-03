8b5067d1d0fd0cd6e97e6627b2807561edb88047
ERERkin <erkin1399@gmail.com>
2020-06-04 01:38:36 +0600

    Create Project

---
+++ src/test/java/kg/itAcademy/migration/MigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.itAcademy.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
